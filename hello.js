0f563e8b883ec15cdb6c2325b8f057a556a79140
Katalina Caldera <katalinacaldera19@gmail.com>
2024-04-23 15:50:24 -0500

    add some js

---
+++ hello.js
@@ -0,0 +1,9 @@
+// turm on strict mode to help us with best prsctices and error checking
+"use strict"
+
+// creating a variable called message to hold the message "Hello World"
+let message = "Hello World!";
+
+// displaying the message varibable in the console
+console.log(message);
+
